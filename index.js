db526efb869217d78429239e3627146f1afa4047
Jenny60234 <dssh30906@gmail.com>
2025-07-28 23:48:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const line = require('@line/bot-sdk');
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const app = express();
 
@@ -405,6 +404,7 @@ app.listen(port, async () => {
   
   // 初始化測試資料
   await initializeTestData();
+  // Fixed dotenv issue for Railway deployment
 });
 
 module.exports = app;
